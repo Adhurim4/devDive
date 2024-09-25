407bf3f8ea215495b3a682e04dcfa8bb8fe10dcf
Adhurim-Systems3000 <amuqiqi@systems3000.com>
2024-09-25 14:10:10 +0200

    initial commit

---
+++ src/pages/JobsPages.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import JobListings from '../components/JobListings'
+
+
+const JobsPages = () => {
+  return (
+    <section className=''>
+
+        <JobListings/>
+    </section>
+  )
+}
+
+export default JobsPages
